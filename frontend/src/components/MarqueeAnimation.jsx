f24ed378ad6ea1ddfbf23079323f0a962e6c73dd
Gagan Sharma <gagansharma3002@gmail.com>
2024-09-21 23:52:58 +0530

    Marque Debugged

--- frontend/src/components/MarqueeAnimation.jsx
+++ frontend/src/components/MarqueeAnimation.jsx
@@ -1,63 +1,62 @@
 // MarqueeAnimation.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import MarqueeItem from './MarqueeItem';
 
-
 const MarqueeAnimation = () => {
+  const marqueeRef = useRef(null);
 
   useEffect(() => {
+    const marquee = marqueeRef.current;
+    const marqueeWidth = marquee.offsetWidth;
+    const itemWidth = marquee.firstChild.offsetWidth;
+    const itemsNeeded = Math.ceil(window.innerWidth / itemWidth) + 1;
+
+    // Clone items to create a continuous loop
+    for (let i = 0; i < itemsNeeded; i++) {
+      marquee.appendChild(marquee.firstChild.cloneNode(true));
+    }
+
+    const animate = (reverse = false) => {
+      gsap.to(marquee, {
+        x: reverse ? 0 : -itemWidth,
+        duration: 5,
+        ease: 'none',
+        repeat: -1,
+        onRepeat: () => {
+          gsap.set(marquee, { x: reverse ? -itemWidth : 0 });
+        },
+      });
+    };
+
     const handleScroll = (e) => {
+      gsap.killTweensOf(marquee);
       if (e.deltaY > 0) {
-        gsap.to('.marque', {
-          x: '-200%',
-          duration: 2 ,
-          repeat: -1,
-          ease: 'none',
-        });
-        gsap.to('.marque img', {
-          rotate: 180,
-        });
+        animate();
       } else {
-        gsap.to('.marque', {
-          x: '0%',
-          duration:2,
-          repeat: -1,
-          ease: 'none',
-        });
-        gsap.to('.marque img', {
-          rotate: 0,
-        });
+        animate(true);
       }
     };
+
+    // Start the animation
+    animate();
 
     // Add event listener
     window.addEventListener('wheel', handleScroll);
 
-    // Clean up the event listener when the component is unmounted
+    // Clean up
     return () => {
       window.removeEventListener('wheel', handleScroll);
+      gsap.killTweensOf(marquee);
     };
   }, []);
 
   return (
-    
-      <div id="move">
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
-        <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
+    <div className=" flex w-screen overflow-hidden bg-cyan-400 h-20 text-center">
+      <div ref={marqueeRef} className="flex">
         <MarqueeItem text="TAKE THE TEST" imgSrc="./arrow-br.svg" />
       </div>
-    
+    </div>
   );
 };
 
--- frontend/src/components/MarqueeItem.jsx
+++ frontend/src/components/MarqueeItem.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const MarqueeItem = ({ text, imgSrc }) => {
   return (
-    <div className="marque">
-      <h1>{text}</h1>
-      <img src={imgSrc} alt="img" />
+    <div className="flex items-center whitespace-nowrap px-4">
+      <h1 className="text-lg font-bold">{text}</h1>
+      <img src={imgSrc} alt="img" className="ml-2 h-6 w-6" />
     </div>
   );
 };
